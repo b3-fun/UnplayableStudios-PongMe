52c1d0fb9837b19815a112b6f22ec6f44f284d79
lubojanski <woooj23@gmail.com>
2025-02-07 16:26:43 +0100

    lint

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,7 @@
-import {gameEnv, gameParams} from './globals';
-import {gameStateType, ballType} from './types';
-import {Server} from 'socket.io';
-import {basementSendCustomActivity, updateForWinner} from './basement.util';
+import { gameEnv, gameParams } from "./globals";
+import { gameStateType, ballType } from "./types";
+import { Server } from "socket.io";
+import { basementSendCustomActivity, updateForWinner } from "./basement.util";
 
 // Returns true if min <= num <= max
 function between(num: number, min: number, max: number) {
@@ -23,12 +23,12 @@ export const collides = (ball: ballType, playerX: number, playerY: number) => {
   };
 
   if (
-      between(ballCenter.x, player.left, player.right) &&
-      between(ballCenter.y, player.top, player.bottom)
+    between(ballCenter.x, player.left, player.right) &&
+    between(ballCenter.y, player.top, player.bottom)
   ) {
     const collidePoint =
-        (ballCenter.y - (player.top + gameEnv.paddleHeight / 2)) /
-        (gameEnv.paddleHeight / 2);
+      (ballCenter.y - (player.top + gameEnv.paddleHeight / 2)) /
+      (gameEnv.paddleHeight / 2);
     const angle = (collidePoint * Math.PI) / 4;
     const direction = ball.x < gameEnv.tableCenter.x ? 1 : -1;
     ball.vx = Math.ceil(direction * ball.speed * Math.cos(angle));
@@ -48,14 +48,16 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
   function moveBall() {
     game.ball.x += game.ball.vx;
     game.ball.y += game.ball.vy;
-    io.to(roomName).emit('locationUpdate', {
+    io.to(roomName).emit("locationUpdate", {
       playerNumber: 0,
-      newLocation: {x: game.ball.x, y: game.ball.y},
+      newLocation: { x: game.ball.x, y: game.ball.y },
     });
   }
 
   // Checks if ball collides with wall or paddle (called on each frame).
   function collisionCheck() {
+    console.log("game :", game);
+
     let hit;
     if (game.ball.x < gameEnv.tableCenter.x) {
       hit = collides(game.ball, game.p2.x, game.p2.y);
@@ -63,7 +65,7 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
       hit = collides(game.ball, game.p1.x, game.p1.y);
     }
     if (hit) {
-      io.to(roomName).emit('collision');
+      io.to(roomName).emit("collision");
     }
   }
 
@@ -76,17 +78,17 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
     game.ball.speed = gameParams.ballVelocity.v;
     game.p1.y = gameEnv.p1Location.y;
     game.p2.y = gameEnv.p2Location.y;
-    io.to(roomName).emit('locationUpdate', {
+    io.to(roomName).emit("locationUpdate", {
       playerNumber: 0,
-      newLocation: {x: game.ball.x, y: game.ball.y},
+      newLocation: { x: game.ball.x, y: game.ball.y },
     });
-    io.to(roomName).emit('locationUpdate', {
+    io.to(roomName).emit("locationUpdate", {
       playerNumber: 1,
-      newLocation: {x: game.p1.x, y: game.p1.y},
+      newLocation: { x: game.p1.x, y: game.p1.y },
     });
-    io.to(roomName).emit('locationUpdate', {
+    io.to(roomName).emit("locationUpdate", {
       playerNumber: 2,
-      newLocation: {x: game.p2.x, y: game.p2.y},
+      newLocation: { x: game.p2.x, y: game.p2.y },
     });
   }
 
@@ -106,7 +108,7 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
     if (p1Scored) game.p1.score += 1;
     if (p2Scored) game.p2.score += 1;
     if (p1Scored || p2Scored) {
-      io.to(roomName).emit('scoreUpdate', {
+      io.to(roomName).emit("scoreUpdate", {
         s1: game.p1.score,
         s2: game.p2.score,
       });
@@ -121,68 +123,82 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
       try {
         basementSendCustomActivity({
           launcherJwt: game.p1.token,
-          label: getWinnerMessage(game.p1.name, game.p2.name, game.p1.score, game.p2.score),
-          eventId: 'match'
-        }).then(()  =>console.log('sent'));
-
+          label: getWinnerMessage(
+            game.p1.name,
+            game.p2.name,
+            game.p1.score,
+            game.p2.score
+          ),
+          eventId: "match",
+        }).then(() => console.log("sent"));
       } catch (error) {
-        console.error('Winner activity failed:', error);
+        console.error("Winner activity failed:", error);
       }
 
       try {
-        updateForWinner(game.p1.token).then(()  =>console.log('sent'));
+        updateForWinner(game.p1.token).then(() => console.log("sent"));
       } catch (error) {
-        console.error('updateForWinner activity failed:', error);
+        console.error("updateForWinner activity failed:", error);
       }
 
       //send to player 2
       try {
         basementSendCustomActivity({
           launcherJwt: game.p2.token,
-          label: getLoserMessage(game.p1.name, game.p2.name, game.p1.score, game.p2.score),
-          eventId: 'match'
-        }).then(()  =>console.log('sent'));
-
+          label: getLoserMessage(
+            game.p1.name,
+            game.p2.name,
+            game.p1.score,
+            game.p2.score
+          ),
+          eventId: "match",
+        }).then(() => console.log("sent"));
       } catch (error) {
-        console.error('Loser activity failed:', error);
+        console.error("Loser activity failed:", error);
       }
 
-      io.to(roomName).emit('winnerUpdate', {winnerNumber: 1});
+      io.to(roomName).emit("winnerUpdate", { winnerNumber: 1 });
       game.p1.paused = true;
-
-
     } else if (game.p2.score === gameParams.winningScore) {
       //send to player 2
       try {
         basementSendCustomActivity({
           launcherJwt: game.p2.token,
-          label: getWinnerMessage(game.p2.name, game.p1.name, game.p2.score, game.p1.score),
-          eventId: 'match'
-        }).then(()  =>console.log('sent'));
-
+          label: getWinnerMessage(
+            game.p2.name,
+            game.p1.name,
+            game.p2.score,
+            game.p1.score
+          ),
+          eventId: "match",
+        }).then(() => console.log("sent"));
       } catch (error) {
-        console.error('Winner activity failed:', error);
+        console.error("Winner activity failed:", error);
       }
 
       try {
-        updateForWinner(game.p2.token).then(()  =>console.log('sent'));
+        updateForWinner(game.p2.token).then(() => console.log("sent"));
       } catch (error) {
-        console.error('updateForWinner activity failed:', error);
+        console.error("updateForWinner activity failed:", error);
       }
 
       //send to player 1
       try {
         basementSendCustomActivity({
           launcherJwt: game.p1.token,
-          label: getLoserMessage(game.p2.name, game.p1.name, game.p2.score, game.p1.score),
-          eventId: 'match'
-        }).then(()  =>console.log('sent'));
-
+          label: getLoserMessage(
+            game.p2.name,
+            game.p1.name,
+            game.p2.score,
+            game.p1.score
+          ),
+          eventId: "match",
+        }).then(() => console.log("sent"));
       } catch (error) {
-        console.error('Loser activity failed:', error);
+        console.error("Loser activity failed:", error);
       }
 
-      io.to(roomName).emit('winnerUpdate', {winnerNumber: 2});
+      io.to(roomName).emit("winnerUpdate", { winnerNumber: 2 });
       game.p1.paused = true;
     }
   }
@@ -192,11 +208,10 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
     basementSendCustomActivity({
       launcherJwt: game.p1.token,
       label: getStartGameMessage(game.p2.name),
-      eventId: 'match'
-    }).then(()  =>console.log('sent'));
-
+      eventId: "match",
+    }).then(() => console.log("sent"));
   } catch (error) {
-    console.error('Live activity failed', error);
+    console.error("Live activity failed", error);
   }
 
   //send to player 2
@@ -204,26 +219,33 @@ export const playGame = (io: Server, roomName: string, game: gameStateType) => {
     basementSendCustomActivity({
       launcherJwt: game.p2.token,
       label: getStartGameMessage(game.p1.name),
-      eventId: 'match'
-    }).then(()  =>console.log('sent'));
-
+      eventId: "match",
+    }).then(() => console.log("sent"));
   } catch (error) {
-    console.error('Live activity failed', error);
+    console.error("Live activity failed", error);
   }
 
-
-  function getStartGameMessage(player2: string){
+  function getStartGameMessage(player2: string) {
     return `@{username} vs ${player2} - Game On!`;
   }
 
-  function getWinnerMessage(winner: string, loser: string, winnerScore: number, loserScore: number){
-    return `@{username} Victory! You defeated ${loser} ${winnerScore}-${loserScore}`
+  function getWinnerMessage(
+    winner: string,
+    loser: string,
+    winnerScore: number,
+    loserScore: number
+  ) {
+    return `@{username} Victory! You defeated ${loser} ${winnerScore}-${loserScore}`;
   }
 
-  function getLoserMessage(winner: string, loser: string, winnerScore: number, loserScore: number){
+  function getLoserMessage(
+    winner: string,
+    loser: string,
+    winnerScore: number,
+    loserScore: number
+  ) {
     return `@{username} Match ended: ${winner} won ${winnerScore}-${loserScore}`;
   }
-
 
   // Game loop
   game.mainLoop = setInterval(() => {
